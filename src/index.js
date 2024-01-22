08c9cd4737f05681cff1c3787674438564fbf03e
Maria Echavarria <maria.echavarria191@pascualbravo.edu.co>
2024-01-22 16:14:46 -0500

    Api rest with NodeJS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const v1WorkoutRouter = require("./v1/routes/workoutroutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors()); // Habilita CORS para todas las rutas
 
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
